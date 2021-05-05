d62105180d466381491c604a5cc65bcb30ae665c
Dongwon 🙉 <dongwonkim@Dongwonui-MacBookPro.local>
2021-05-06 01:32:28 +0900

    qa 3차 진행중

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("HI");
+// console.log(window);
+
+// $(window).resize(function () {
+var width = window.innerWidth;
+console.log(width);
+$(window)
+  .resize(() => {
+    if (width > 800) {
+      $("#banner").attr("src", "./asset/banner2.png");
+    }
+  })
+  .resize();
+//   console.log(width);
+// })();
